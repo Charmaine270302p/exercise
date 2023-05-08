3a8c007bb616e2422d413b4e3dc1f4ec56c28b07
“Charmaine270302p”  <charmainemwandiringa@gmail.com>
2023-05-08 11:14:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+ const pi = 3.14
+console.log(pi); 
+  
+ 
+var myName = 'Charmy';
+var myAge = 10;
+var myDetails = console.log(`My name is ${myName} and l am ${myAge} years old.`);
+
+
+const number = (x, y) => console.log(x + y); 
+number(3,4);
+number(4,5);
+
+
+function  sum(a,b) {
+  console.log(a * b);
+}
+sum(4,5);
+sum(10,5); 
+
+
+ var name = 'John';
+ var str ='Hello, ${name}!'; 
+
+var greeting = console.log('Hello,${name}!'); 
+
+
+
