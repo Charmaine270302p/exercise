c78ec4e5fd460538559c6703cc875c0450ea4926
“Charmaine270302p”  <charmainemwandiringa@gmail.com>
2023-05-09 11:00:55 +0200

    terminal commands

--- script.js
+++ script.js
@@ -27,5 +27,12 @@ var name  = 'John';
 console.log('Hello ' +  name)
 
 
+/* Terminal commands
+1. Cd-allows you to change the current directory.
+2. Dir-this command lists all the files and directories in the current directory.
+3. Mkdir-creates a new directory
+4. Cp-make copy of a file.
+5. Del-deletes a file.
+6. Tasklist-lists all the currently running processes on your system */
 
 
